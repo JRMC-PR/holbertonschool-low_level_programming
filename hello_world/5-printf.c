88b95acfd29d46b815ea737cbd393125d7efc542
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 18:23:11 +0000

    error fixing 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main- Execute
+ *printf- Print the messae to stdout
+ *reurn 0- indicates success aslong as it is 0 
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
