27c695490ac9016a42544ea8b8a4e7993e730f90
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 16:20:14 +0000

    4. a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Main function*/
+int man()
+{ 
+	/*Stores the valus fo the string to be passed to puts*/
+	char output[] = "\"Programming is like building a multilingual puzzle";
+	puts(output);//outputs the string saved in output 
+	return 0;
+}
