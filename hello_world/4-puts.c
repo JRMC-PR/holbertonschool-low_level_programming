87500b9cec1ee66e493f512fdfa3b3ee6f304472
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 16:36:14 +0000

    error fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*Main function*/
+/*Main function executes the next block og code inside the {}*/
 int main(void)
 {
 	/*Stores the valus fo the string to be passed to puts*/
-	char output[] = "\"Programming is like building a multilingual puzzle\n";
+	char output[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(output);/*outputs the string saved in output*/
 
