0463c0018f5eb1675c4e10b049e94c386e1aef6a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 17:24:05 +0000

    error fixing

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	long long int w;
 	float o;
 
-	printf("The byte size of a char is:",sizeof(c));
-	printf("The byte size of a int is:",sizeof(i));
-	printf("The byte size of a long int is:",sizeof(r));
-	printf("The byte size of a long long int is:",sizeof(w));
-	printf("The byte size of a flot is:",sizeof(o));
+	printf("The byte size of a char is:%d.\n",sizeof(c));
+	printf("The byte size of a int is:%d.\n",sizeof(i));
+	printf("The byte size of a long int is:%d.\n",sizeof(r));
+	printf("The byte size of a long long int is:%d.\n",sizeof(w));
+	printf("The byte size of a flot is:%d.\n",sizeof(o));
 
 	return (0);
 }
