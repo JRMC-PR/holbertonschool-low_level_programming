77faf6dc99696162eba16b4334e907d9e2cb73e9
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 18:57:13 +0000

    9. #Advanced a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- Execute
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char out[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(out);
+
+	return (1);
+}
