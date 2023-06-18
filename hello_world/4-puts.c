622288bbfff659c481a72c6547bc96e866144858
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 16:42:48 +0000

    error fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /*
  * Main function executes the next block og code inside the {}
- * */
+ *
+ * 
+ * 
+ * /
 int main(void)
 {
 	/*Stores the valus fo the string to be passed to puts*/
