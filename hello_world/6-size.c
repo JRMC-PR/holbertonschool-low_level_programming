67a941ef1b26ac92c818d4c6917a720877bec91a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 17:29:26 +0000

    error fixing 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-	char c; 
+	char c;
 	int i;
 	long int r;
 	long long int w;
 	float o;
 
-	printf("Size of a char : %d byte(s).\n",sizeof(c));
-	printf("Size of an int : %d btyte(s).\n",sizeof(i));
-	printf("Size of a long int : %d byte(s).\n",sizeof(r));
-	printf("Size of a long long int : %d byte(s).\n",sizeof(w));
-	printf("Size of a float : %d byte(s).\n",sizeof(o));
+	printf("Size of a char : %d byte(s).\n", sizeof(c));
+	printf("Size of an int : %d btyte(s).\n", sizeof(i));
+	printf("Size of a long int : %d byte(s).\n", sizeof(r));
+	printf("Size of a long long int : %d byte(s).\n", sizeof(w));
+	printf("Size of a float : %d byte(s).\n", sizeof(o));
 
 	return (0);
 }
