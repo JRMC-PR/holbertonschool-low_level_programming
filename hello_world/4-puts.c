16aced4c439c6bed91f87ad2a9de8212bd23b00a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 17:01:32 +0000

    error fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
+/*
+ *Main: Executes the program
+ *
+ *Return: if ok is 0
+ */
 int main(void)
 {
-/*
- *The main fuction will execute
- *everything inside the {}
- *
- *and return a 0 if everythign is ok
- *
- *cahr output hilds the output string
- *
- */
 	char output[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(output);
