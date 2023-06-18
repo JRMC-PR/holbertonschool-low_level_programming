51afbf4af2f50cdf9174946450978caffb2d111f
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 17:04:38 +0000

    error fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	char output[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(output);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
