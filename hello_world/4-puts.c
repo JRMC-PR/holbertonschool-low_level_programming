b25198a21da101ad234c494dc9446e1c1b32f471
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-18 16:22:45 +0000

    error fix 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,6 +4,6 @@ int main()
 { 
 	/*Stores the valus fo the string to be passed to puts*/
 	char output[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(output);//outputs the string saved in output 
+	puts(output);/*outputs the string saved in output*/ 
 	return 0;
 }
