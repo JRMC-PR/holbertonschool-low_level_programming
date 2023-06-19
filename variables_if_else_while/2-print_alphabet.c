b6de484aa68c63bd894df33790ef98590d4bea73
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-19 18:52:52 +0000

    error fix

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stido.h>
+/**
+ *main- Execute
+ *Return: if 0 success
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char a;
+
+	for(i=97;i <= 122; i++)
+	{
+		a = i + '0'
+		putchar(a);
+	}
+
+	return (0);
+
+}
