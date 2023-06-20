3abe684d0f111f7ba8b97268db24ff8cf7cca21d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-20 18:34:40 +0000

    1. a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main- Execute
+ *Return: 0 ia success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i < 123 ; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+	return (0);
+}
