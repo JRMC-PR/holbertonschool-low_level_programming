fba76a2a04cd4fe761457e51ea39284d6c833856
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-20 00:03:45 +0000

    4. a program that prints the alphabet in lowercase, followed by a new line.

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main- Execute
+ *Return: 0 if success
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+
+	puthcar('\n');
+
+	return (0);
+}
+
+
