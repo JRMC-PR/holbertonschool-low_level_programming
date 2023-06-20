da5083a307cd1540c5c9e8dc57ee98326840fdd1
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-20 22:38:43 +0000

    5. a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sing.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *print_sign- evaluate the function
+ *@n: stores number value to bess passed
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("%d", n);
+		return (0);
+	}
+	else
+	{
+		putchar(45);
+		return (1);
+	}
+}
