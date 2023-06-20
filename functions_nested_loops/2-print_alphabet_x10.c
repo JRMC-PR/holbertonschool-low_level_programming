13498011133efb35146ae03daafa2525feff3be2
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-20 18:58:36 +0000

    2.  a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet_x10- Execute
+ *Return: No return because is void
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 97 ; j <123 ; j++)
+			_putchar(j);
+
+		_putchar('\n');
+	}
+
+}
