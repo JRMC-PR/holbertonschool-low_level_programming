c3ae62b0589dfc5d9b8ca7884364f9aec2c0ee02
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-21 18:51:15 +0000

    9. a function that prints the 9 times table, starting with 0.

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table- execute function
+ *
+ *
+ */
+void times_table(void)
+{
+	int r, c;
+
+	for (r = 0 ; r < 10 ; r++)
+	{
+		for (c = 0 ; c < 10 ; c++)
+		{
+			_putchar((r * a ) + '0');
+			_putchar(44);
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+}
