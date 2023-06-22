9f38f90e8395459f3d30ea46b4909822bac2e0ed
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-22 11:49:14 +0000

    9. a function that prints the 9 times table, starting with 0.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,29 +7,29 @@
  */
 void times_table(void)
 {
-	int r, c, F, L;
+	int r, c;
 
 	for (r = 0 ; r < 10 ; r++)
 	{
 		for (c = 0 ; c < 10 ; c++)
 		{
-			if ((r * c) < 10)
+			if ((r * c) >= 10)
+			{
+				_putchar((r * c) / 10 + '0');
+				_putchar((r * c) % 10 + '0');
+				_putchar(44);
+				_putchar(' ');
+			}else 
 			{
 				_putchar((r * c) + '0');
 				_putchar(44);
 				_putchar(' ');
-			}
-			else
-			{
-				F = (r * c) / 10;
-				L = (r * c) % 10;
-				_putchar(F + '0');
-				_putchar(L + '0');
-				_putchar(44);
-				_putchar(' ');
 				_putchar(' ');
 			}
+
+			_putchar('\n');
+			
+			
 		}
-		_putchar('\n');
 	}
 }
