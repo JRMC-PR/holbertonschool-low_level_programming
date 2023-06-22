ada4d484010f8014c948f463c98089aa60336c3d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-22 18:13:40 +0000

    11. function that prints all natural numbers from n to 98, followed by a new line.

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void print_to_98(int n)
 		for (i = n ; i < 99 ; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i);
+				printf("%d, \n", i);
 			else
-				printf("%d", i);
+				printf("%d\n", i);
 
 		}
 	}
@@ -24,9 +24,9 @@ void print_to_98(int n)
 			for (i = n ; i > 97 ; i--)
 			{
 			if (i != 98)
-				printf("%d, ", i);
+				printf("%d, \n", i);
 			else
-				printf("%d", i);
+				printf("%d\n", i);
 			}
 	}
 }
