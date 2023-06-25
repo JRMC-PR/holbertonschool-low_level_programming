71a1f4f05af6c1fc9bf74dee4ec2609f2110e045
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-25 13:25:43 +0000

    9. program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int i, j, m3, m5;
+	char fb[]="FizzBuzz";
+
+
+	for (i = 0; i < 101; i++)
+	{
+		m3 = i % 3;
+		m5 = i % 5;
+
+		if (m3 == 0 && m5 == 0)
+		{
+			for (j = 0; j < sizeof(fb); j++)
+			{
+				_putchar(fb[i]);
+			}
+			_putchar(' ');
+		}
+		else if (m3 == 0 && m5 != 0)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				_putchar(fb[i]);
+			}
+		}
+		else if (m3 != 0 && m5 == 0)
+		{
+			for (j = 4; j < sizeof(fb); j++)
+			{
+				_putchar(fb[i]);
+			}
+		}
+		else if (i >= 10)
+		{
+			_putcha((i / 10) + '0');
+			_putchar((i % 10) +'0');
+		}
+		else
+		{
+			_putchar(i);
+		} /*end if else if */
+	} /*end outer for*/
+	_putchar('\n');
+} /*end main */
