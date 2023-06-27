ae1dd30398e8995902129c9da8157a4116c94fdb
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-27 00:59:37 +0000

    3. a function that prints a string, followed by a new line, to stdout.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_puts- Executes function
+ *@str: hol a string
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i] + '0');
+	}
+	
+}
