67e428697ba215c1d056f64677332f4815c5f0b5
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-29 18:53:22 +0000

    a function that copies a string.

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int size = 0 , j = 0;
+	int size = 0, j = 0;
 
-	while(dest[size])
+	while (dest[size])
 	{
 		size++;
 	}
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
 
-	for(; j < n; j++)
+	for (; j < n; j++)
 		dest[j] = '\0';
+
 	return (dest);
 }
