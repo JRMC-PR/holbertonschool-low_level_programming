d99432aaaf058ca93b278225cb19fe7759cec57c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-29 12:23:53 +0000

    a function that concatenates two strings.

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	{
 		size++;
 	}
-	for (j = 0; j < size && src[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	dest[size + 1] = src[size];
 
 	dest[size + 1] = '\0';
