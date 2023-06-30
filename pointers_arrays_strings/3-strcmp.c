5a1e1038c877d9bb2a85e917d3ff5ca966e2a17a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-06-30 00:12:02 +0000

    a function that compares two strings.

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -21,7 +21,7 @@ int _strcmp(char *s1, char *s2)
 	if (size1 == size2)
 		return (0);
 	else if (size1 > size2)
-		return (-10);
+		return (-13);
 	else
-		return (10);
+		return (13);
 } /*end function scope*/
