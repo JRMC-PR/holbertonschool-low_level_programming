567722a915f96a6e7661347c4e4ad3cd95ab933e
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-01 17:40:23 +0000

    a function that encodes a string into 1337.

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ **leet- function name
+ *@srt: points to a string
+ *Return: str pointer
+ */
+char *leet(char * str)
+{
+	/*Variables*/
+	int i = 0, j  = 0, half;
+	char checkers[10] = { 'A', 'a', 'E', 'e', 'o', 'O', 't', 'T', 'l', 'L'};
+	char num[5] = {'4', '3', '0', '7', '1'};
+	/*Loops will handle the movement of the array*/
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == checkers[j])
+			{
+				half = j / 2;
+				str[i] = num[half];
+			} /*end if*/
+		} /*end inner for*/
+	} /*end outer for*/
+	/*add null at the end*/
+	str[i + 1] = '\0';
+	return (str);
+} /*end function*/
