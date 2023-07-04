30000ea7a2fce3f7ba6687ab4e23d14747cbd5b9
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-04 20:46:36 +0000

    a function that returns the length of a string.

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  *int_strlen_recursion- Executes function
  *@s: points to a char
- *Return
+ *Return: recursive function or nul to exit
  */
 int _strlen_recursion(char *s)
 {
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-	return (1 + _strlen_recursion(s + 1 ));
+	return (1 + _strlen_recursion(s + 1));
 } /*end function*/
