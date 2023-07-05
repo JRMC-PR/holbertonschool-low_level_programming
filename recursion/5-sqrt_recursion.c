1c1ad0c0700f12ca40207f57c98f78c24617296a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-05 18:05:38 +0000

    a function that returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_sqrt_recursion- Execute function
+ *@n: hold an int
+ *Return: return 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	} /*end if else */
+	return (_sqrt_helper(0, n));
+} /*end function*/
+
+/**
+ *_sqrt_helper- executes helper function
+ *@sq: holds an int
+ *@n: holds an int
+ *Return: recursive or result
+ */
+int _sqrt_helper(int sq, int n)
+{
+	if (sq * sq < n)
+	{
+		return (_sqrt_helper(sq + 1, n));
+	}
+	else if (sq *sq > n)
+	{
+		return (-1);
+	}
+	return (sq);
+} /*end helper function*/
+
+
+
+
+
--- recursion/main.h
+++ recursion/main.h
@@ -33,6 +33,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt_helper(int sq, int n);
 int is_prime_number(int n);
 int is_div_by(int n, int div); /*helper function for isPrime*/
 #endif
