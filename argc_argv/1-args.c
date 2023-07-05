10e574b25c88901dae94c91c9e1f76732e57724d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-05 20:09:24 +0000

    a program that prints the number of arguments passed into it.

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ *main- Execute main function
+ *@argc: holds an int
+ *@argv: points to a pointer array
+ *Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
+	return (0);
 }
