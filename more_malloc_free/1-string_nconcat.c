68d8854dd092af3c713f94d6796ea181aae31161
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-15 18:11:43 +0000

    a function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,9 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (concat == NULL)
 		return (NULL);
 	/*concat data*/
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < s1; i++)
 		concat[i] = s1[i];
-	for (j =0; j < n ; j++)
+	for (j = 0; j < n ; j++)
 		concat[i++] = s2[j];
 	/*add nullbyte*/
 	concat[strlen(concat) + 1] = '\0';
