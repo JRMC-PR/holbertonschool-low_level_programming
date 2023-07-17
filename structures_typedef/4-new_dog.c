0971b4632383c8cd9712a66cf12e112e138b3c10
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-17 19:35:53 +0000

    dog_t *new_dog(char *name, float age, char *owner);

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,24 +19,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	} /*end if*/
-	new_dog->name = malloc(strlen(name) * sizeof(name));
-	if (new_dog->name != NULL)
-		new_dog->name = strdup(name);
-	else
+	new_dog->name = malloc(strlen(name) * sizeof(char));
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 	/*end if else for name*/
-	new_dog->age = age;
-	new_dog->owner = malloc(strlen(owner) * sizeof(owner));
-	if (new_dog->owner != NULL)
-		new_dog->owner = strdup(owner);
-	else
+	new_dog->owner = malloc(strlen(owner) * sizeof(char));
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	} /*end if else for owner*/
+
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
 	return (new_dog);
 } /*end function*/
