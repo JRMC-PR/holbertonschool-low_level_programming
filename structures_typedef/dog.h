fb1ac8530e0d9ad511d9b1b79e46c413cfc14f82
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-17 14:11:56 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c -o a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog -a structure that identifies a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Creates a struct the will hold the att od a dog
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
