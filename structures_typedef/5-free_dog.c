8953af03d6d158e516e6bfe4ea37082469306758
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-17 19:45:24 +0000

    a function that frees dogs.

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog- frees allocated struct in memory
+ *@d: points to the dog struct
+ */
+void free_dog(dog_t *d)
+{
+	/*free allocated meory*/
+	free(d->name);
+	free(d->owner);
+	free(d);
+} /*end function*/
+
