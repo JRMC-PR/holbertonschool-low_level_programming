954b17a2d32da4af81d2797dde9061c83ec5a290
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-24 13:37:13 +0000

    a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,49 +1,84 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h>
 /**
- * print_all - function that prints anything
- * @format: is a list of types of argument passed to the function
+ *print_all- a function that prints anything pased to it
+ *@format: a constant char pointer
  */
 void print_all(const char * const format, ...)
 {
-	va_list a;
-	int i = 0, j = 0;
-	char *s_let = NULL;
-
-	va_start(a, format);
-	while (format && format[j])
-		j++;
-	while (format && format[i])
+	/*Variables*/
+	unsigned int i = 0, j = 0;
+	char *separator = "";
+	f func_type[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list args;
+	/*start argument iteratons*/
+	va_start(args, format);
+	while (format != NULL && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (j < 4)
 		{
-			case 'c':
-				printf("%c", va_arg(a, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(a, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(a, double));
-				break;
-			case 's':
-				s_let = va_arg(a, char *);
-				if (s_let == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", s_let);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		if (i != j - 1)
-			printf(", ");
+			/*verify if the cases match anything we have*/
+			if (format[i] == *func_type[j].id)
+			{
+				func_type[j].func(separator, args);
+				separator = ", ";
+			} /*end if*/
+			j++;
+		} /*end inner for*/
 		i++;
-	}
+	} /*end outer for*/
+	va_end(args);
 	printf("\n");
-	va_end(a);
-}
+} /*end fucntion*/
+
+/**
+ *print_char- a function that prints a char
+ *@separator: points to a char with something assigned
+ *@args: argument of the list
+ */
+void print_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+} /*end char*/
+
+/**
+ *print_int- a function that prints an int
+ *@separator: points to a char with something assigned
+ *@args: argument of the list
+ */
+void print_int(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+} /*end int*/
+
+/**
+ *print_float- a function that prints an float
+ *@separator: points to a char with something assigned
+ *@args: argument of the list
+ */
+void print_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+} /*end float*/
+
+/**
+ *print_string- a function that prints an string
+ *@separator: points to a char with something assigned
+ *@args: argument of the list
+ */
+void print_string(char *separator, va_list args)
+{
+	char *string;
+	string = va_arg(args, char *);
+	/*chec if string is empty*/
+	if (args == NULL)
+		string = "(nil)";
+	printf("%s%s", separator, string);
+} /*end string*/
