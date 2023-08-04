de2a040ddfa199deecda8deb733df0b42d7f082a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-04 12:51:41 +0000

    a function that returns the sum of all the data (n) of a dlistint_t linked list.

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *sum_dlisint - sums the data of a int list
+ *@head: points to a list
+ *Return: sum or null if failed
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	/*Declarations*/
+	int sum = 0;
+	/*check if head is empty*/
+	if (head == NULL)
+		return (0);
+	/*move and sum*/
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	} /*end while*/
+	return (sum);
+} /*end function*/
