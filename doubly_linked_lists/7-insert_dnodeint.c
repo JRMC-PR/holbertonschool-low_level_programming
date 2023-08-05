98e3f448891d0931a2d591866f7b69531eb38874
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-05 00:13:08 +0000

    a function that inserts a new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h)->n = n;
 		return (new_node);
 	}
+	/*if idx is 0*/
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 	new_node->n = n; /*set n value*/
 	/*start moving to find the location*/
 	while (temp != NULL && count != idx)
