322e15228879ae31eb82b6be4622a6ee8cf91ccc
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-06 17:43:59 +0000

    a function that prints a triangle, followed by a new line.

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,29 +3,32 @@
 /**
  *print_triangle- execute the function
  *@size: honls an int
- *
  */
 void print_triangle(int size)
 {
+	/*Declarations*/
 	int i, k;
-
+	/*Check if size if less than 0*/
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+	else /*create the triangle */
 	{
-		for (i = 1; i < size + 1; i++)
+		for (i = 1; i <= size ; i++)
 		{
 			k = 0;
-			while(k <= size -i)
+			while(k < size - i)
 			{
 				_putchar(' ');
 				k++;
 			}
-			while(
-			_putchar(35);
+			while (k != size)
+			{
+				_putchar(35);
+				k++;
+			} /*fill the rest of the line with #*/
 			_putchar('\n');
-		}
+		} /*end outter for*/
 	}/*end if else*/
 } /*end function*/
