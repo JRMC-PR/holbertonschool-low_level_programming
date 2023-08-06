2a6dc85df9d5a682a39929ca427ea8cb13ce080b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-06 16:17:17 +0000

    a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,7 @@ void more_numbers(void)
 			{
 				_putchar((i / 10) + '0');
 			}
-			else
-				_putchar((i % 10) + '0');
+			_putchar((i % 10) + '0');
 		} /*end inner for*/
 		_putchar('\n');
 	} /*end outter for*/
