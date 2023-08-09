ddf5162c415d808b50aee1ecbf188b6f9ab915b4
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-09 17:22:45 +0000

    a  function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,6 @@ int create_file(const char *filename, char *text_content)
 	/*Verify if text_content NULL*/
 	if (fd == -1)
 		return (-1);
-	
 	/*write to the file*/
 	if (text_content != NULL)
 	{
