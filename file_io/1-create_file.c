33dc04f51c2aa3485d863cb8eee8ae3113acab8c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-09 17:09:32 +0000

    a  function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,6 +25,6 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	} /*end if*/
 	/*write to the file*/
-	write(fd, text_content, text_size);
+	write(STDOUT_FILENO, text_content, text_size);
 	return (1);
 } /*end function*/
