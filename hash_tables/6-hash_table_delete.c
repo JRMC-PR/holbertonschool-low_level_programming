5f9f19e304c7f9359e33da23b5e46ff70a521dd6
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-14 00:23:55 +0000

    a function that delets a has table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -13,10 +13,13 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		hash_node_t *current = ht->array[i];
+
 		while (current != NULL)
 		{
 			hash_node_t *temp = current;
+
 			current = current->next;
+
 			free(temp->key);
 			free(temp->value);
 			free(temp);
