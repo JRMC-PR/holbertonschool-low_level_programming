abd5b3ed85365c921d77a6183237f8c40ea74a0a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-01 14:36:35 -0400

    commits adds andvanded taks

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -7,38 +7,51 @@
  *@argv: an array of pinters that are pointing to char's
  *Return: 0 (Success), 1 (Error);
  */
+int cent_count(int money);
+
 int main(int argc, char *argv[])
 {
 	/*Variables*/
-	int i = 0,cents = 0, total_sum = 0;
-	/*verify how much data is stored*/
-	if (agrc != 1)
+	int i = 0, total_sum = 0;
+	/*verify input*/
+	if (argc != 2)
 	{
 		printf("ERROR\n");
 		return (1);
-	}
+	} /*end if */
+	/*verify input value*/
 	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");
 		return (0);
-	}
-	
+	} /*end oif */
+	printf("%d\n", cent_count(atoi(argv[1])));
 	/*divide add 25 untill value surpaces the amout of cents
 	 * then move to the next coin to do the same*/
 } /*end main function*/
 /**
  *cent_count- counts the cents
- *@cent: holds the total amout we need to  change
- *@change: holds an int
+ *@cmoney: holds the total amout we need to  change
  */
-int cent_count(int cent, int change)
+int cent_count(int money)
 {
-
-	while (cent < changes)
+	/*variables*/
+	int change = 0, sum = 0, i = 0;
+	int cents[5] = {25, 10, 5, 2, 1};
+	/*calculate the change*/
+	while (sum != money)
 	{
-		
-	}
-	return (0);
+		/*sum the cent value to the total */
+		sum += cents[i];
+		change++;
+		/*check lif the sum is > */
+		if (sum > money)
+		{
+			/*revert to prev ammount*/
+			sum -= cents[i];
+			change--;
+			i++; /* next position in cents*/
+		}		 /*end if*/
+	}			 /*end while*/
+	return (change);
 } /*end helper function*/
-
-
